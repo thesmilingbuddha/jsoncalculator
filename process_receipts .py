4b173ad3201ac6f78ed6cd3ccb12589644057998
Siddhanth Madhusudhan <sid210@ymail.com>
2018-11-28 23:02:27 +0530

    Added the json calculator

---
+++ process_receipts .py
@@ -0,0 +1,23 @@
+import os
+import glob
+import shutil
+import json
+
+try:
+    os.mkdir("./processed")
+except OSError:
+    print("Processed directory already exists")
+
+receipts = glob.glob("./new/receipt-[0-9]*.json")
+subtotal = 0.0
+
+for path in receipts:
+    with open(path) as f:
+        content = json.load(f)
+        subtotal += float(content ['value'])
+    name = path.split("/")[-1]
+    destination = f"./processed/{name}"
+    shutil.move(path, destination)
+    print(f"moved '{path}' to '{destination}'")
+
+print("Receipt subtotal: $%.2f" %subtotal)
